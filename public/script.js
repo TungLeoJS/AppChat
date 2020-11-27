c08370b38defcb4e3bc02498965c67095896c126
gtlduytung@gmail.com <gtlduytung@gmail.com>
2020-11-28 00:13:59 +0700

    fix bug

--- public/script.js
+++ public/script.js
@@ -62,13 +62,9 @@ navigator.mediaDevices
       stream,
       Object.keys(peers)[Object.values(peers).indexOf(myPeer._id)]
     );
-<<<<<<< HEAD
-    // addUserName(Object.keys(peers)[Object.values(peers).indexOf(myPeer._id)]);
-=======
-    setTimeout(() => {
-      addUserName(Object.keys(peers)[Object.values(peers).indexOf(myPeer._id)]);
-    }, 500);
->>>>>>> 45ae3ca8e98638918e94d57cdfa90a7a6297d267
+    // setTimeout(() => {
+    //   addUserName(Object.keys(peers)[Object.values(peers).indexOf(myPeer._id)]);
+    // }, 500);
     myPeer.on("call", (call) => {
       const callerName = call.metadata.callerName;
       peers[callerName] = call.peer;
